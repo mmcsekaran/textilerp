66d059abc5bfd502e0ba4bbb1feee6a67cced694
mmcsekaran <37895707+mmcsekaran@users.noreply.github.com>
2022-06-18 09:36:56 +0530

    Update Addresscomponent.js

--- src/Modules/Common/Addresscomponent.js
+++ src/Modules/Common/Addresscomponent.js
@@ -1,43 +1,12 @@
-import { Input, Select,Form, Card} from 'antd'
-import TextArea from "antd/lib/input/TextArea";
+import { Form } from 'antd'
 import React, { Component } from 'react'
 
-export default class Addresscomponent extends Component {
+export default class AddressComponent extends Component {
   render() {
-    return (<>
-    
-      <Form
-       >
-
-          <Form.Item
-          label={'Address'}
-          >
-              <TextArea />
-          </Form.Item>
-          <Form.Item
-          label={'City'}
-          >
-              <Select />
-          </Form.Item>
-          <Form.Item
-          label={'State'}
-          >
-              <Select />
-          </Form.Item>
-          <Form.Item
-          label={'PinCode'}
-          >
-              <Input />
-          </Form.Item>
-
-          <Form.Item
-          label={'Country'}
-          >
-              <Select />
-          </Form.Item>
+    return (
+      <Form>
+          
       </Form>
-      
-      </>
     )
   }
 }
